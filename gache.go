ead797a266597ad01050ab6d05165b5831039b83
yusukato <yusukato@yahoo-corp.jp>
2018-07-18 13:06:35 +0900

    fix

--- gache.go
+++ gache.go
@@ -43,10 +43,12 @@ func init() {
 	GetGache()
 }
 
+// New returns Gache (*gache) instance
 func New() Gache {
 	return newGache()
 }
 
+// newGache returns *gache instance
 func newGache() *gache {
 	g := &gache{
 		data:   new(sync.Map),
@@ -56,6 +58,7 @@ func newGache() *gache {
 	return g
 }
 
+// GetGache returns Gache (*gache) instance
 func GetGache() Gache {
 	once.Do(func() {
 		instance = newGache()
@@ -63,19 +66,23 @@ func GetGache() Gache {
 	return instance
 }
 
+// isValid checks expiration of value
 func (v *value) isValid() bool {
 	return v.expire == 0 || time.Now().UnixNano() < v.expire
 }
 
+// SetDefaultExpire set expire duration
 func (g *gache) SetDefaultExpire(ex time.Duration) Gache {
 	g.expire.Store(ex)
 	return g
 }
 
+// SetDefaultExpire set expire duration
 func SetDefaultExpire(ex time.Duration) {
 	instance.SetDefaultExpire(ex)
 }
 
+// StartExpired starts delete expired value daemon
 func (g *gache) StartExpired(ctx context.Context, dur time.Duration) Gache {
 	go func() {
 		tick := time.NewTicker(dur)
@@ -92,6 +99,7 @@ func (g *gache) StartExpired(ctx context.Context, dur time.Duration) Gache {
 	return g
 }
 
+// ToMap returns All Cache Key-Value map
 func (g *gache) ToMap() map[interface{}]interface{} {
 	m := make(map[interface{}]interface{})
 	g.data.Range(func(k, v interface{}) bool {
@@ -109,10 +117,12 @@ func (g *gache) ToMap() map[interface{}]interface{} {
 	return m
 }
 
+// ToMap returns All Cache Key-Value map
 func ToMap() map[interface{}]interface{} {
 	return instance.ToMap()
 }
 
+// get returns value & exists from key
 func (g *gache) get(key interface{}) (interface{}, bool) {
 
 	v, ok := g.data.Load(key)
@@ -131,14 +141,17 @@ func (g *gache) get(key interface{}) (interface{}, bool) {
 	return *d.val, true
 }
 
+// Get returns value & exists from key
 func (g *gache) Get(key interface{}) (interface{}, bool) {
 	return g.get(key)
 }
 
+// Get returns value & exists from key
 func Get(key interface{}) (interface{}, bool) {
 	return instance.get(key)
 }
 
+// set sets key-value & expiration to Gache
 func (g *gache) set(key, val interface{}, expire time.Duration) {
 	var exp int64
 	if expire != 0 {
@@ -150,30 +163,37 @@ func (g *gache) set(key, val interface{}, expire time.Duration) {
 	})
 }
 
+// SetWithExpire sets key-value & expiration to Gache
 func (g *gache) SetWithExpire(key, val interface{}, expire time.Duration) {
 	g.set(key, val, expire)
 }
 
-func (g *gache) Set(key, val interface{}) {
-	g.set(key, val, g.expire.Load().(time.Duration))
-}
-
+// SetWithExpire sets key-value & expiration to Gache
 func SetWithExpire(key, val interface{}, expire time.Duration) {
 	instance.set(key, val, expire)
 }
 
+// Set sets key-value to Gache using default expiration
+func (g *gache) Set(key, val interface{}) {
+	g.set(key, val, g.expire.Load().(time.Duration))
+}
+
+// Set sets key-value to Gache using default expiration
 func Set(key, val interface{}) {
 	instance.set(key, val, instance.expire.Load().(time.Duration))
 }
 
+// Delete deletes value from Gache using key
 func (g *gache) Delete(key interface{}) {
 	g.data.Delete(key)
 }
 
+// Delete deletes value from Gache using key
 func Delete(key interface{}) {
 	instance.Delete(key)
 }
 
+// DeleteExpired deletes expired value from Gache it can be cancel using context
 func (g *gache) DeleteExpired(ctx context.Context) <-chan int {
 	c := make(chan int)
 	go func() {
@@ -196,10 +216,12 @@ func (g *gache) DeleteExpired(ctx context.Context) <-chan int {
 	return c
 }
 
+// DeleteExpired deletes expired value from Gache it can be cancel using context
 func DeleteExpired(ctx context.Context) <-chan int {
 	return instance.DeleteExpired(ctx)
 }
 
+// Foreach calls f sequentially for each key and value present in the Gache.
 func (g *gache) Foreach(f func(interface{}, interface{}, int64) bool) Gache {
 	g.data.Range(func(k, v interface{}) bool {
 		d, ok := v.(*value)
@@ -215,10 +237,12 @@ func (g *gache) Foreach(f func(interface{}, interface{}, int64) bool) Gache {
 	return g
 }
 
+// Foreach calls f sequentially for each key and value present in the Gache.
 func Foreach(f func(interface{}, interface{}, int64) bool) Gache {
 	return instance.Foreach(f)
 }
 
+// Clear deletes all key and value present in the Gache.
 func (g *gache) Clear() {
 	g.data.Range(func(key, _ interface{}) bool {
 		g.data.Delete(key)
@@ -227,6 +251,7 @@ func (g *gache) Clear() {
 	g.data = new(sync.Map)
 }
 
+// Clear deletes all key and value present in the Gache.
 func Clear() {
 	instance.Clear()
 }
